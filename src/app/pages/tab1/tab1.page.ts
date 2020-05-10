f6c43b2b5edaef3f40a726b1aa02826c7ca3cba2
martinlugani <tincholan10@hotmail.com>
2020-05-10 13:23:53 -0300

    version 1 cap 98 Antes de hacer modulos funciona

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NoticiasService } from '../../sevrices/noticias.service';
+import { Article } from '../../interfaces/interfaces';
 
 @Component({
   selector: 'app-tab1',
@@ -8,6 +9,11 @@ import { NoticiasService } from '../../sevrices/noticias.service';
 })
 // la interface on init permite cargar en la pagina durante el evento del inicio
 export class Tab1Page implements OnInit {
+  // aqu determino la obtecion de cada articulo para pasarselo a el html
+noticias: Article[] = [];
+
+
+
 // aqui obtengo el servicio para obtener los valores que obtiene este de la api
   constructor(private noticiasServices: NoticiasService) {}
   ngOnInit() {
@@ -15,6 +21,8 @@ export class Tab1Page implements OnInit {
     this.noticiasServices.getTopHeadLines()
     .subscribe(resp => {
       console.log('noticias', resp);
+      // en la linea  que sigue lo que se hace es cargar el arreglo
+      this.noticias.push(... resp.articles);
     });
   }
 
